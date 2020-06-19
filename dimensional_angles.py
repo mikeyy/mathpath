8265f07a72449c3320cd6eabfc5f8d6da1a576d9
Michael Mooney <mikeyy@mikeyy.com>
2020-06-19 16:23:18 -0500

    Update dimensional_angles.py

--- dimensional_angles.py
+++ dimensional_angles.py
@@ -1,20 +1,14 @@
-import pprint
-
 base = 3
 steps = 10
 first_dimension = ((steps-1) *  (steps-2)) * steps
 second_dimension = (steps  + (steps - 1)) * (steps - 1)
 
-angles = []
-for step in range(3, first_dimension):
-    result = step ** 2
-    results = []
-    for step in range(steps + 3, second_dimension):
-        result = step * (result/steps)
-        results.append(result)
-    angles.append(results)
+result = second_dimension ** 2
+for step in range(steps + 3, second_dimension):
+    result = (step * (result))
 
-pprint.pprint(angles)
+print(result)
+
 
 '''
 waves = sound_files[0]
