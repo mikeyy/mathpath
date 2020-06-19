dd2fdcf662643aa4ab521c86483376009f3d48c2
Michael Mooney <mikeyy@mikeyy.com>
2020-06-19 16:40:44 -0500

    Update dimensional_angles.py

--- dimensional_angles.py
+++ dimensional_angles.py
@@ -7,7 +7,14 @@ result = second_dimension ** 2
 for step in range(steps + 3, second_dimension):
     result = (step * (result))
 
-print(result)
+# Forever loop (Large calculations)
+
+base = 3
+steps = base ** base
+angles = ((steps-1) * (steps-2)) ** steps
+while 1:
+    angles = angles ** steps
+    print(len(str(angles)))
 
 
 '''
