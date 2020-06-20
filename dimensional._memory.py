8246fbdef06ff91c9cb8543f0638430c23ccef7d
Michael Mooney <mikeyy@mikeyy.com>
2020-06-19 23:00:19 -0500

    Update dimensional._memory.py

--- dimensional._memory.py
+++ dimensional._memory.py
@@ -1,11 +1,13 @@
-# Looking for dimensional memory
+# Looking for dimensional memory (memory in math)
 
 import time
 
 base_step = 10
 bases = 3
 steps = ((bases * bases) * (base_step)) / (base_step * base_step)
+count = 1
 while 1:
     steps = ((steps ** 2) ** steps) / (base_step * 2)
-    print(steps)
+    print(steps, count)
+    count += 1
     time.sleep(1)
