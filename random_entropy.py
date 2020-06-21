87bea813b4c07c23a6961f9938aeaf9ec6893283
Michael Mooney <mikeyy@mikeyy.com>
2020-06-20 21:33:58 -0500

    Update random_entropy.py

--- random_entropy.py
+++ random_entropy.py
@@ -6,8 +6,9 @@ bases = 3
 all_steps = []
 all_bases = range(bases, bases_step)
 for base in all_bases:
-    all_steps.append(((base * base) * (bases_step))
+    all_steps.append(((base * base) * (bases_step))) 
 
 while 1:
     for step in all_steps:    
-        step = (step ** step) * random.random()
+        step = (step ** step ** step ** step ** step ** step)
+        print(step)
